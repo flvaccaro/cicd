2205a7e6bf7600467065935aeb913a214fc2dd8b
Leonel <flvaccaro@hotmail.com>
2021-06-14 22:10:13 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          El pendejo me come los dos huevos <code>html</code>
         </p>
         <a
           className="App-link"
